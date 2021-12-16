81f20cffe2eb458939c612f4bd3604f7100d88fa
vsalihu2 <vsalihu2@gmail.com>
2021-12-15 18:22:21 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,13 +1,13 @@
 from . import Expense
 
 class BudgetList():
-    def __init__(self,budget)
+    def __init__(self,budget):
         self.budget = budget
         self.sum_expenses = 0
         self.expenses = []
         self.sum_overages = 0
         self.overages = []
-    def append(self, item)
+    def append(self, item):
         if (self.sum_expenses+item < self.budget):
             self.expenses.append(item)
             self.sum_expenses += item

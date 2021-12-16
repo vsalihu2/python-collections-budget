68eaa2a1877dccae7f8d66dc50cb7f706f990c4a
vsalihu2 <vsalihu2@gmail.com>
2021-12-15 18:03:00 -0700

    Update FrequentExpenses.py

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -12,7 +12,7 @@ spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
 
-fix, ax = plt.subplots()
+fig, ax = plt.subplots()
 ax.bar(categories, count)
 ax.set_title('# of Purchases by Category')
 plt.show()
